0d89ef6e0dee948b87b0940b840ec669af5593eb
Joshua Santos <joshua.santos1@students.mq.edu.au>
2022-09-02 14:06:45 +1000

    added sender name above their messages and a dummy react button

--- src/components/Message.js
+++ src/components/Message.js
@@ -14,22 +14,41 @@ function Message ({msg, onClick, user}) {
     }
 
     return(
-        <>
+        <div style={style.container}>
+            <h6 style={style.creator}><strong>{msg.creator}</strong></h6>
             <li className="msg" style={style.message} data-id={msg.id} onClick={showButton}>
                 {msg.text}
             </li>
-            {button ? <button onClick={() => onClick(msg.id)}>Delete?</button> : <></>}
-        </>
+            {button ? <>
+            <button style={style.button} onClick={() => onClick(msg.id)}>Delete?</button> 
+            <button style={style.button} onClick={() => onClick(msg.id)}>react</button>
+            </> : <></>}
+        </div>
     )
 }
 
 const style = {
+    container :{
+        paddingBottom: "8px"
+    },
+
     message: {
-        position: "relative",
         listStyle: "none",
         maxWidth: "75ch",
         wordBreak: "break-all"
     },
+
+    creator: {
+        marginBottom: "-1px"
+    },
+
+    button: {
+        fontSize: "70%"
+    },
+
+    reaction: {
+
+    }
 }
 
 
